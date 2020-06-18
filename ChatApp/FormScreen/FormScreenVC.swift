f9e41b9022eb6c91c558959c87a0ffa081d2251d
mamhmoud Abdelwahab <mahmoud.elziany@gmail.com>
2020-06-18 11:10:24 +0200

    Initial Commit
    Co-Authored-By: Mahmoud-Abdelwahab <mahmoud-abdelwahab@users.noreply.github.com>

--- ChatApp/FormScreen/FormScreenVC.swift
+++ ChatApp/FormScreen/FormScreenVC.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class FormScreenVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
---
+++ ChatApp/MainChatRoom/MainChatRoomPresenter.swift
@@ -0,0 +1,9 @@
+//
+//  MainChatRoomPresenter.swift
+//  ChatApp
+//
+//  Created by kasper on 6/18/20.
+//  Copyright © 2020 Mahmoud.kasper.ChatApp. All rights reserved.
+//
+
+import Foundation
