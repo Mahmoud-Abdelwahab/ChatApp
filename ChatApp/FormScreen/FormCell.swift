b7c7c486d0a3cc419ca5b6378825c1aa2d9cc942
mamhmoud Abdelwahab <mahmoud.elziany@gmail.com>
2020-06-19 04:37:16 +0200

    Finishing Form Screen Design
    Co-Authored-By: Mahmoud-Abdelwahab <mahmoud-abdelwahab@users.noreply.github.com>

---
+++ ChatApp/FormScreen/FormCell.swift
@@ -0,0 +1,27 @@
+//
+//  FormCell.swift
+//  ChatApp
+//
+//  Created by kasper on 6/18/20.
+//  Copyright © 2020 Mahmoud.kasper.ChatApp. All rights reserved.
+//
+
+import UIKit
+
+class FormCell: UICollectionViewCell {
+ 
+    @IBOutlet weak var signInBtnOutlite: UIButton!
+    
+    @IBOutlet weak var signUpBtnOutlite: UIButton!
+    
+    @IBOutlet weak var passwordField: UITextField!
+       
+       @IBOutlet weak var userNameField: UITextField!
+    
+       @IBOutlet weak var emailField: UITextField!
+       
+     
+    @IBOutlet weak var userNameView: UIView!
+    
+
+}
--- ChatApp/FormScreen/FormScreenVC.swift
+++ ChatApp/FormScreen/FormScreenVC.swift
@@ -8,13 +8,64 @@
 
 import UIKit
 
-class FormScreenVC: UIViewController {
-
+class FormScreenVC: UIViewController , UICollectionViewDelegate , UICollectionViewDataSource , UICollectionViewDelegateFlowLayout  {
+ 
+    @IBOutlet weak var formCollectionView: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        formCollectionView.delegate = self
+        formCollectionView.dataSource = self
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+          return 2
+      }
+      
+      func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+       
+          let cell = self.formCollectionView.dequeueReusableCell(withReuseIdentifier: "FormCell", for: indexPath) as! FormCell
+          
+          if indexPath.row == 0 // signIn
+          {
+            cell.userNameView.isHidden = true
+            cell.signInBtnOutlite.setTitle(" SignUp 👉🏻", for: .normal)
+            cell.signUpBtnOutlite.setTitle("Login", for: .normal)
+            cell.signInBtnOutlite.addTarget(self, action: #selector(slideToSignInCell(_:)), for: .touchUpInside)
+          }else if (indexPath.row == 1){ /// sign Up cell
+             cell.userNameView.isHidden = false
+            cell.signInBtnOutlite.setTitle("<-- Login", for: .normal)
+                       cell.signUpBtnOutlite.setTitle("SignUp", for: .normal)
+             cell.signInBtnOutlite.addTarget(self, action: #selector(slideToSignUpCell(_:)), for: .touchUpInside)
+        }
+          
+          return cell
+      }
+      
+      //UICollectionViewDelegateFlowLayout to change collectionview cell size
+      
+      func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+          return collectionView.frame.size  // cell size = it's parent size which is collection view
+      }
+      
+
+    @objc func slideToSignInCell(_ sender :UIButton) {
+        let indexPath = IndexPath(row: 1, section: 0)
+        self.formCollectionView.scrollToItem(at: indexPath, at: [.centeredHorizontally], animated: true)
+    }
+    
+    /////
+    
+    @objc func slideToSignUpCell(_ sender :UIButton) {
+        let indexPath = IndexPath(row: 0, section: 0)
+        self.formCollectionView.scrollToItem(at: indexPath, at: [.centeredHorizontally], animated: true)
     }
 
 
 }
 
+extension FormScreenVC {
+  
+    
+    
+}
